048442b32f460d3483e1d912bce54da6226fd9b2
kellogsricekrispy <79350471+kellogsricekrispy@users.noreply.github.com>
2021-10-30 09:45:44 -0400

    made mute button work and responsive

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 $(document).ready(()=>{
 
+    // functions for button clicls
+    function toggleMute(){
+        let sound = document.getElementById('video');
+        
+        sound.muted==true ? sound.muted = false : sound.muted = true;
+        console.log(sound.muted)
+    }
+
+    function toggleClose(){
+        let menu = document.getElementsByClassName('toggle');
+        
+    }
+
+    // Jquery that triggers buttons
     $('#sound').click(()=>{
-        $('.video').removeClass('muted');
+        toggleMute();
     })
 
 });
